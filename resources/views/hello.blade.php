fa96f1e484dd75368b7e44f8ff3e13929b9cf583
Marc <mcalafellsmx@gmail.com>
2016-10-27 21:04:06 +0200

    adding footter and scripts layouts

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,7 +1,21 @@
 @extends('layout.App)
 
+@section(htmlhead)
+
+    @include('partials.htmlheader')
+
+@endsection
+
+
+
 @section('content')
 
+hello world
 
+@endsection
+
+@section(htmlfooter)
+
+    @include('partials.htmlfooter')
 
 @endsection
--- resources/views/layouts/App.blade.php
+++ resources/views/layouts/App.blade.php
@@ -1,17 +1,9 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AdminLTE</title>
 
-    <link rel="stylesheet" href="css/all.css">
-</head>
 <body>
 
-@yield("")
+@yield("content")
 
 </body>
 </html>
